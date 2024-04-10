5661360394d70973ded6ede57fa5ffe363dd13b8
mardokh <ahoura.mardokh@gmail.com>
2024-04-10 03:31:30 +0200

    beystore

--- front/src/pages/Public/ProductDetails.js
+++ front/src/pages/Public/ProductDetails.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from "react"
-import { useNavigate, useParams } from "react-router-dom"
+import { useParams } from "react-router-dom"
 import { productService } from "../../_services/product.service"
 import "./product_details.css"
 
@@ -9,7 +9,10 @@ const ProductDetails = () => {
     // STATES
     const [product, setProduct] = useState([])
     const [isLoad, setISload] = useState(false)
-    const navigate = useNavigate()
+    const [SideImage, setSideImage] = useState("")
+    const [sideImageActive, setSideImageActive] = useState(false)
+    const [styleActive, setStyleActive] = useState(false)
+    const [imageIndex, setImageIndex] = useState()
 
 
     // GET ID PARAMS
@@ -26,7 +29,7 @@ const ProductDetails = () => {
         if (flag.current === false) {
             productService.getOneProduct(id)
             .then(res => {
-                console.log(res)
+                //console.log(res)
                 setProduct(res.data.data)
                 setISload(true)
             })
@@ -34,23 +37,32 @@ const ProductDetails = () => {
         }
         return () => flag.current = true
     }, [])
+
+
+    // DISPLAY SIDE IMAGE TO MAIN //
+    const cornerImageDisplay = (image, index) => {
+        setImageIndex(index)
+        setStyleActive(true)
+        setSideImage(image)
+        setSideImageActive(true)
+    }
     
 
     // LOADING //
     if (!isLoad) {
         return <div>Loading ...</div>
     }
+    
 
-   
     return (
         <div className="details_global_container">
                 <div className="details_sideImg_img_container">
                     <div className="details_side_img">
                         {product.product_images.map((file, index) => (
-                            <div key={index} className="details_sideImg_img" style={{backgroundImage: `url('http://localhost:8989/uploads/${file.images}')`}}></div>
+                            <div key={index} className={styleActive && index === imageIndex ? "details_sideImg_img_active" : "details_sideImg_img"} style={{backgroundImage: `url('http://localhost:8989/uploads/${file.images}')`}} onClick={() => cornerImageDisplay(file.images, index)}></div>
                         ))}
                     </div>
-                    <div className="details_img_container" style={{backgroundImage: `url('http://localhost:8989/uploads/${product.image}')`}}></div>
+                    <div className="details_img_container" style={{backgroundImage: `url('http://localhost:8989/uploads/${sideImageActive ? SideImage : product.image}')`}}></div>
                 </div>
                 <div className="details_info_container">
                     <p className="details_name">{product.name}</p>
